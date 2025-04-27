30bdd870abb3ed3937b847b3971770aead438104
faviconico1910 <23521179@gm.uit.edu.vn>
2025-04-27 12:29:41 +0700

    edit function for students

--- Backend/API-Controllers/student_profile.controller.js
+++ Backend/API-Controllers/student_profile.controller.js
@@ -1,35 +1,86 @@
-    const db = require('../config/db.js');
-    const jwt = require("jsonwebtoken");
+const db = require('../config/db.js');
+const jwt = require("jsonwebtoken");
 
-    const profile = async (req, res) => {
-        try {
-            const authHeader = req.headers.authorization; //lấy token từ header
-            console.log("Token nhận được từ client:", authHeader);
-            
-            if (!authHeader || !authHeader.startsWith("Bearer ")) { // kiểm tra token có hợp lệ không
-                return res.status(403).json({ message: "Không có token hoặc token không hợp lệ!" });
-            }
-            const token = authHeader.split(" ")[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            const userId = decoded.Tai_Khoan;  // MSSV lấy từ token
-
-            console.log(" Nhận request student_profile với id từ token:", userId);
-
-            const sql = "SELECT * FROM SINHVIEN WHERE Ma_Sinh_Vien = ?";
-            const [result] = await db.query(sql, [userId]);
-    
-            if (result.length === 0) {
-                console.log("Không tìm thấy sinh viên!");
-                return res.status(404).json({ message: "Không tìm thấy sinh viên!" });
-            }
-    
-            console.log("Dữ liệu sinh viên:", result[0]);
-            res.json(result[0]);
-    
-        } catch (error) {
-            console.error("Lỗi xác thực token hoặc truy vấn:", error);
-            res.status(401).json({ message: "Token không hợp lệ hoặc truy vấn lỗi!" });
+const profile = async (req, res) => {
+    try {
+        const authHeader = req.headers.authorization; //lấy token từ header
+        console.log("Token nhận được từ client:", authHeader);
+        
+        if (!authHeader || !authHeader.startsWith("Bearer ")) { // kiểm tra token có hợp lệ không
+            return res.status(403).json({ message: "Không có token hoặc token không hợp lệ!" });
         }
-    };
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.Tai_Khoan;  // MSSV lấy từ token
 
-    module.exports = { profile };
+        console.log(" Nhận request student_profile với id từ token:", userId);
+
+        const sql = "SELECT * FROM SINHVIEN WHERE Ma_Sinh_Vien = ?";
+        const [result] = await db.query(sql, [userId]);
+
+        if (result.length === 0) {
+            console.log("Không tìm thấy sinh viên!");
+            return res.status(404).json({ message: "Không tìm thấy sinh viên!" });
+        }
+
+        console.log("Dữ liệu sinh viên:", result[0]);
+        return res.json(result[0]);
+
+    } catch (error) {
+        console.error("Lỗi xác thực token hoặc truy vấn:", error);
+        res.status(401).json({ message: "Token không hợp lệ hoặc truy vấn lỗi!" });
+    }
+};
+
+const updateProfile = async (req, res) => {
+    try {
+        const authHeader = req.headers.authorization; //lấy token từ header
+        console.log("Token nhận được từ client:", authHeader);
+        
+        if (!authHeader || !authHeader.startsWith("Bearer ")) { // kiểm tra token có hợp lệ không
+            return res.status(403).json({ message: "Không có token hoặc token không hợp lệ!" });
+        }       
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.Tai_Khoan; // lấy Tai_Khoan từ token
+
+        console.log("Nhận PUT update_profile với id từ token:", userId);
+        const {
+            Ho_Ten,
+            Email_Ca_Nhan,
+            Ngay_Sinh,
+            So_CMND,
+            Ton_Giao,
+            Gioi_Tinh,
+            Thuong_Tru
+        } = req.body;
+        const ngaySinhFormatted = Ngay_Sinh ? new Date(Ngay_Sinh).toISOString().split('T')[0] : null;
+        const sql = `
+            UPDATE SINHVIEN
+            SET Ho_Ten = ?, Email_Ca_Nhan = ?, Ngay_Sinh = ?, So_CMND = ?, Ton_Giao = ?, Gioi_Tinh = ?, Thuong_Tru = ?
+            WHERE Ma_Sinh_Vien = ?
+        `;
+        const [result] = await db.query(sql, [
+            Ho_Ten,
+            Email_Ca_Nhan,
+            ngaySinhFormatted,
+            So_CMND,
+            Ton_Giao,
+            Gioi_Tinh,
+            Thuong_Tru,
+            userId
+        ]);
+        
+        console.log("Kết quả cập nhật:", result);
+        // trả về client
+        const [newProfile] = await db.query("SELECT * FROM SINHVIEN WHERE Ma_Sinh_Vien = ?", [userId]);
+        return res.json(newProfile[0]);
+    }
+    catch (error) {
+        console.error("Lỗi cập nhật profile:", error);
+    }
+}
+
+module.exports = { profile, updateProfile};
+
+
--- Backend/routes/student.routes.js
+++ Backend/routes/student.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require('path')
-const { profile } = require("../API-Controllers/student_profile.controller.js");
+const { profile, updateProfile }= require("../API-Controllers/student_profile.controller.js");
 const router = express.Router();
 
 // api
 router.get("/api", profile);
-
+router.put("/api", updateProfile);
 // frontend
 router.get("/profile", (req, res) => {
     res.sendFile(path.join(__dirname, "../../Frontend", "profile.html"));
