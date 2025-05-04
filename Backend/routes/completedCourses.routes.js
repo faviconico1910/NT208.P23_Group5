84a7b4d44b3835d9be463006edc740480ba444a2
khieulam28 <khieubaolam28@gmail.com>
2025-05-04 08:36:35 +0700

    Sua api cho ket qua hoc tap

--- Backend/routes/completedCourses.routes.js
+++ Backend/routes/completedCourses.routes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getCompletedCourses } = require("../API-Controllers/completedCourses.controller"); // Kiểm tra lại đường dẫn này
+const { getCompletedCourses,  getCompletedCoursesByMSSV } = require("../API-Controllers/completedCourses.controller"); // Kiểm tra lại đường dẫn này
+const path = require("path");
 
-router.get("/", getCompletedCourses);
+router.get("/api", getCompletedCourses);
+
+router.get("/KetQuaHocTap", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/KetQuaHocTap", "completedCourses.html"));
+});
+
 module.exports = router;
